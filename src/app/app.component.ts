5789a8230780c236ec2b7d5db8a8efba81f0c8b1
tancs2 <158019347+tancs1@users.noreply.github.com>
2024-02-01 12:05:53 +0500

    weather app in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CommonService} from '../app/comman.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-app';
+  weatherData: any;
+  isChangeLocation=false;
+  todayDate: Date;
+
+
+  city='fasialabad'
+  constructor(private service:CommonService){
+this.todayDate=new Date()
+  }
+  getlocation(location:any){
+    this.getWeather(location);
+
+  }
+  ngOnInit(): void {
+    this.getWeather('faisalabad');
+  }
+ 
+  changeLocation(){
+    console.log("button click");
+    debugger
+this.isChangeLocation=this.isChangeLocation==true?false:true;
+  }
+// app.component.ts
+getWeather(city: string): void {
+  this.service.getWeather(city).subscribe((data: any) => {
+    console.log('Icon Code:', data.weather[0]?.icon);
+    this.weatherData = data;
+
+  });
+}
+
+  getWeatherDescription(weatherArray: any[]): string {
+    if (weatherArray && weatherArray.length > 0) {
+      return weatherArray[0].description;
+    }
+    return 'N/A';
+  }
+  getRainValue(data: any): string {
+    // Check if 'rain' is present directly or within 'weather'
+    if (data.rain) {
+      return data.rain['24h'] ? data.rain['24h'] + ' mm' : 'N/A';
+    } else if (data.weather && data.weather[0].rain) {
+      return data.weather[0].rain['24h'] ? data.weather[0].rain['24h'] + ' mm' : 'N/A';
+    }
+    return 'N/A';
+  }
+  getFormattedTime(timestamp: number | undefined): string {
+    if (timestamp) {
+      const date = new Date(timestamp * 1000); // Convert from Unix timestamp to milliseconds
+      return date.toLocaleTimeString();
+    }
+    return 'N/A';
+  }
+
+
+  
+  
 }
---
+++ src/app/comman.service.ts
@@ -0,0 +1,19 @@
+// weather.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+  private apiKey = '95e68bc624eacfe3843422fc005c2947';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`; // units=metric for Celsius
+    return this.http.get(url);
+  }
+}
